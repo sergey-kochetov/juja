84becacbe01b54f1298d6d42f61a99e9f3f846ed
sergey-kochetov <git.kochetov@gmail.com>
2018-10-24 19:10:28 +0300

    sort

---
+++ jujacore/src/main/java/sorted/BubbleSorter.java
@@ -0,0 +1,17 @@
+package sorted;
+
+public class BubbleSorter {
+    public static void sort(int[] arr) {
+        for (int barrier = arr.length - 1; barrier >= 0; barrier--) {
+            for (int index = 0; index < barrier; index++) {
+                if (arr[index] > arr[index + 1]) {
+                    int tmp = arr[index];
+                    arr[index] = arr[index + 1];
+                    arr[index + 1] = tmp;
+                }
+            }
+        }
+    }
+
+
+}
