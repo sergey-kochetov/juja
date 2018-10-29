a22ccd0697aa87e9308e68baa24c32c86498c7c4
sergey-kochetov <git.kochetov@gmail.com>
2018-10-29 10:30:35 +0300

    oop 1

---
+++ jujacore/src/main/java/com/juja/oop/Cargo.java
@@ -0,0 +1,30 @@
+package com.juja.oop;
+
+public class Cargo extends AbstractShip {
+    private float tonnage;
+    public static final float DEFAULT_RENTAL=550;
+
+    public Cargo(String name, float length, float width, float displacement) {
+        super(name, length, width, displacement);
+    }
+
+    public Cargo(String name, float length, float width, float displacement, float tonnage) {
+        super(name, length, width, displacement);
+        this.tonnage = tonnage;
+    }
+
+    @Override
+    public float calculatePayment() {
+        return DEFAULT_RENTAL * tonnage;
+    }
+
+    public float calculatePayment(float money) {
+        if (money > 0) {
+            return money * tonnage;
+        }
+        return calculatePayment();
+    }
+
+
+
+}
