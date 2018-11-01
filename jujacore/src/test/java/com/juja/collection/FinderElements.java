21f6b608ef8883086b417d2c973b250ced2caf88
sergey-kochetov <git.kochetov@gmail.com>
2018-11-01 18:39:31 +0300

    Simple linked list 3

---
+++ jujacore/src/test/java/com/juja/collection/FinderElements.java
@@ -0,0 +1,21 @@
+package com.juja.collection;
+
+import java.util.Iterator;
+
+public class FinderElements {
+    public static Integer findElement(SinglyLinkedList<Integer> singlyLinkedList, int k) {
+        int count = 0;
+        for (Iterator it = singlyLinkedList.iterator(); it.hasNext(); ) {
+            it.next();
+            count++;
+        }
+        Integer[] result = new Integer[count];
+        int index = 0;
+        for (Iterator it = singlyLinkedList.iterator(); it.hasNext();) {
+            result[index++] = (Integer) it.next();
+        }
+        return result[count - k - 1];
+    }
+
+
+}
