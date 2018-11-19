a63d25d35e77e1c1ab32896fd12819783b5f7cf0
sergey-kochetov <git.kochetov@gmail.com>
2018-11-19 16:20:21 +0300

    reflection - test

---
+++ jujacore/src/test/java/com/juja/reflection/SimpleReflectTest.java
@@ -0,0 +1,21 @@
+package com.juja.reflection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimpleReflectTest {
+    @Test
+    public void test_1() {
+        System.out.println("TEST_1");
+    }
+    @Test
+    public void test_2() {
+        System.out.println("TEST_2");
+    }
+    @Test
+    public void test_3() {
+        System.out.println("TEST_3");
+    }
+
+}
