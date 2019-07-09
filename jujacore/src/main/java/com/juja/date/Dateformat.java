28c63a638c327856e9b18ef9ea80cf915ebd3737
sergey-kochetov <git.kochetov@gmail.com>
2019-07-09 14:33:30 +0300

    simple Calendar class

--- jujacore/src/main/java/com/juja/date/Dateformat.java
+++ jujacore/src/main/java/com/juja/date/Dateformat.java
@@ -2,6 +2,7 @@ package com.juja.date;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -24,6 +25,63 @@ public class Dateformat {
         } catch (ParseException e) {
             e.printStackTrace();
         }
+        System.out.println();
+
+        long millis = System.currentTimeMillis();
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setTimeInMillis(millis);
+        System.out.println("minutes: " + calendar.get(Calendar.MINUTE));
+        System.out.println("hours: " + calendar.get(Calendar.HOUR_OF_DAY));
+
+        System.out.println("day of week: " + calendar.get(Calendar.DAY_OF_WEEK));
+        System.out.println("day of month: " + calendar.get(Calendar.DAY_OF_MONTH));
+        System.out.println("day of year: " + calendar.get(Calendar.DAY_OF_YEAR));
+
+        calendar.add(Calendar.DAY_OF_YEAR, -1);
+        System.out.println("yesterday: " + format.format(calendar.getTime()));
+
+        System.out.println("is leap year 2019: " + isLeapYear(2019));
+        System.out.println("is leap year 2020: " + isLeapYear(2020));
+
+        System.out.println("start of week day: " + calendar.getFirstDayOfWeek());
+        calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
+
+        System.out.println("start of week date: " + format.format(calendar.getTime()));
+
+        System.out.println("is dates of the same day: " + dateOfSameDay(Calendar.getInstance(), Calendar.getInstance()));
+    }
+
+    private static boolean dateOfSameDay(Calendar calendar1, Calendar calendar2) {
+        return calendar1.get(Calendar.YEAR) == calendar2.get(Calendar.YEAR)
+                && calendar1.get(Calendar.DAY_OF_YEAR) == calendar2.get(Calendar.DAY_OF_YEAR);
+    }
+
+    private static String isLeapYear(int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, Calendar.FEBRUARY, 1);
+        return calendar.getActualMaximum(Calendar.DAY_OF_MONTH) == 29 ? "yes" : "no";
+    }
+
+    private static Calendar calendarStartOfDay(Calendar dateTime) {
+        Calendar calendar = (Calendar) dateTime.clone();
+
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar;
+    }
+
+    private static Calendar calendarStartOfMonth(Calendar dateTime) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dateTime.getTime());
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        return calendar;
     }
 
     private static String getReadableDateWeekDay(Date date) {
